2390592bd4a8eb7bd819ae867be5c5f8a568543b
amarjeetsingh7503 <amarjeetsingh75003@gmail.com>
2025-02-14 13:20:33 +0530

    mongo atlas uri

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
